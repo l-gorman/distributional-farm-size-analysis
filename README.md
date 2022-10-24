2b54a28c894ffb4af03a093fac010804a03920cb
l-gorman <lgorman@turing.ac.uk>
2022-10-24 10:41:13 +0100

    updates dependencies

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 
-Was previosly using gdal version:3.5.1_3
+
+
+# Configuration
+
+Make sure to load all the module (if running on HPC) from `data-prep.sh`.
+
+In addition, create a file `~/.R/Makevars` with the following contents:
+
+```
+MAKEFLAGS = -j8
+
+## C++ flags
+CXX=g++
+CXX11=g++
+CXX14=g++
+CXX17=g++
+
+CXXFLAGS=-O3 -march=native -Wno-ignored-attributes
+CXX11FLAGS=-O3 -march=native -Wno-ignored-attributes
+CXX14FLAGS=-O3 -march=native -Wno-ignored-attributes
+CXX17FLAGS=-O3 -march=native -Wno-ignored-attributes
+
+CXXPICFLAGS=-fPIC
+CXX11PICFLAGS=-fPIC
+CXX14PICFLAGS=-fPIC
+CXX17PICFLAGS=-fPIC
+
+CXX11STD=-std=c++11
+CXX14STD=-std=c++14
+CXX17STD=-std=c++17
+
+## C flags
+CC=gcc
+CFLAGS=-O3 -march=native
+
+## Fortran flags
+FC=gfortran
+F77=gfortran
+FFLAGS=-O3 -march=native
+FCFLAGS=-O3 -march=native
+
+
+```
+
+See [here](https://github.com/stan-dev/rstan/issues/892)
+for more details
--- data-prep.sh
+++ data-prep.sh
@@ -23,11 +23,17 @@ echo "Keep track of job by entering sacct -j ${SLURM_JOBID}  \n"
 echo "Cancel your job by entering scancel ${SLURM_JOBID}  \n"
 echo "More details on submitting jobs here https://www.acrc.bris.ac.uk/protected/hpc-docs/job_types/ \n"
 
-module add languages/r/4.1.0
-module add tools/cmake/3.20.0 #Needed for R-package nloptr
-module add tools/gdal/3.4.0 # Needed for spatial analysis
-module add GDAL/2.0.2-foss-2016a
-module add PROJ/4.9.2-foss-2016a
+module add lib/gdal/3.3.0-gcc
+module add lib/geos/3.10.2-gcc
+module add lib/proj/8.2.1
+module add lang/r/4.0.1
+module add lang/gcc/9.1.0
+module add tools/cmake/3.22.2 #Needed for R-package nloptr
+# module add languages/r/4.1.0
+# module add tools/cmake/3.20.0 #Needed for R-package nloptr
+# module add tools/gdal/3.4.0 # Needed for spatial analysis
+# module add GDAL/2.0.2-foss-2016a
+# module add PROJ/4.9.2-foss-2016a
 # d=$(date +%Y-%m-%d)
 
 # export OMP_NUM_THREADS=4
